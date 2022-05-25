6ccb7d76fd8b41d527958cb70fe4fb73c54a7e80
G1e6 <gleb-potapov@rambler.ru>
2022-05-25 20:51:22 +0500

    peremennie

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        int x = 100; // начальная сумма на счёте
-        int y = 1100; //сумма пополнения
-        //int k; //количество бонусов
-        int sum;
-        int kb = y / 100; //количество бонусов
-        if (y > 1000) {
-            sum = x + y + kb;//сумма на счёте, если пополнение больше 1000
+        int nachsumm = 100; // начальная сумма на счёте
+        int summpopolneniya = 1100; //сумма пополнения
+        int sumNaSchete;
+        int kolichestvobonusov = summpopolneniya / 100; //количество бонусов
+        if (summpopolneniya > 1000) {
+            sumNaSchete = nachsumm + summpopolneniya + kolichestvobonusov;//сумма на счёте, если пополнение больше 1000
         } else {
-            sum = x + y;//сумма на счёте, если пополнение меньше или равно 1000
+            sumNaSchete = nachsumm+ summpopolneniya;//сумма на счёте, если пополнение меньше или равно 1000
         }
-        System.out.println("Сумма на счёте = " + x);
-        System.out.println("Количество бонусов = " + kb);
-        System.out.println("Сумма на счёте = " + sum);
+        System.out.println("Сумма на счёте = " + nachsumm);
+        System.out.println("Количество бонусов = " + kolichestvobonusov);
+        System.out.println("Сумма на счёте = " + sumNaSchete);
 
     }
 }
