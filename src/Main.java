9c2c15b21fc2087c8b055acacc2f83689a4a5375
G1e6 <gleb-potapov@rambler.ru>
2022-05-26 19:16:57 +0500

    dell

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        int nachsumm = 100; // начальная сумма на счёте
-        int summpopolneniya = 1100; //сумма пополнения
-        int sumNaSchete;
-        int kolichestvobonusov = summpopolneniya / 100; //количество бонусов
-        if (summpopolneniya > 1000) {
-            sumNaSchete = nachsumm + summpopolneniya + kolichestvobonusov;//сумма на счёте, если пополнение больше 1000
+        int initialAmount = 100; // начальная сумма на счёте
+        int replenishmentAmount = 1100; //сумма пополнения
+        int accountAmount;
+        int numberOfBonuses = replenishmentAmount / 100; //количество бонусов
+        if (replenishmentAmount > 1000) {
+            accountAmount = initialAmount + replenishmentAmount + numberOfBonuses;//сумма на счёте, если пополнение больше 1000
         } else {
-            sumNaSchete = nachsumm+ summpopolneniya;//сумма на счёте, если пополнение меньше или равно 1000
+            accountAmount = initialAmount + replenishmentAmount;//сумма на счёте, если пополнение меньше или равно 1000
         }
-        System.out.println("Сумма на счёте = " + nachsumm);
-        System.out.println("Количество бонусов = " + kolichestvobonusov);
-        System.out.println("Сумма на счёте = " + sumNaSchete);
+        System.out.println("Сумма на счёте = " + initialAmount);
+        System.out.println("Количество бонусов = " + numberOfBonuses);
+        System.out.println("Сумма на счёте = " + accountAmount);
 
     }
 }
